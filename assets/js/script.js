1e67379cd0f3fd47dddd5b898d7511f2bc3ce14e
byt-32 <null@gmail.com>
2021-12-09 20:20:27 -0800

    update on the navbar, changed the position to right

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,9 @@
 	const navbar = document.querySelector('#navbar')
 	const close = document.querySelectorAll('#nav_close > .fa')[0]
 
+	navbar.classList.add('animate__animated')
+	ovlay.classList.add('animate__animated')
+
 	const ul = document.createElement('ul')
 	ovlay.querySelector('nav').appendChild(ul)
 
@@ -13,40 +16,42 @@
 
 	function cloneElements() {
 		const shadw = document.querySelectorAll('.nav_links > li')
-		for (let i = 0; i<shadw.length; i+=1) {
-			const shdw_link = shadw[i].querySelector('a');
+		for (let i = 0; i<shadw.length + 1; i+=1) {
 			const li = document.createElement('li')
 			const a = document.createElement('a')
-			a.setAttribute('href', shdw_link.getAttribute('href'))
-			a.textContent = shdw_link.textContent.toUpperCase()
+			
+			if (i >= shadw.length) {
+				console.log(i, shadw.length)
+				a.setAttribute('href', '#')
+				a.textContent = 'Donate'
+			} else {
+				const shdw_link = shadw[i].querySelector('a');
+				a.setAttribute('href', shdw_link.getAttribute('href'))
+				a.textContent = shdw_link.textContent
 
+			}
 			li.appendChild(a)
 			ul.appendChild(li)
 		}
+
 	}// Clones the original nav elements 
 	cloneElements()
 	
 	//handles the nav properties
 	const nav = ovlay.querySelector('nav')
 	navbar.addEventListener('click', function () {
+	nav.classList.add('animate__animated')
 		const a = ovlay.querySelectorAll('a')
 
-		navbar.classList.add('animate__animated')
-		ovlay.classList.add('animate__animated')
-		nav.classList.add('animate__animated')
+		if (!navbar.getAttribute('active')) {//if the user opens the nav
+			navbar.setAttribute('active', 'true')
 
-
-		if (navbar.getAttribute('active')) {//checks if the nav icon has been clicked to open nav
-			navbar.removeAttribute('active')//if user wants to close the nav
-
-			hideNav()//calls hidenav
-		} else {
-			navbar.setAttribute('active', 'true')//else if user opens the nav
-
-			ovlay.classList.add('animate__slideInRight')	
-			ovlay.classList.remove('animate__fadeOut')
-			
+			ovlay.classList.add('animate__fadeIn')
+			nav.classList.add('animate__fadeInRight')	
 			ovlay.style.visibility = 'visible'
+
+		} else {
+			hideNav()//calls hidenav
 		}
 
 		for (let i = 0; i<a.length;i+=1) {
@@ -55,23 +60,13 @@
 			})
 		}//hide the overlay when a link is clicked
 
-		setTimeout(()=>{
-			ovlay.classList.remove('animate__slideInRight')
-			nav.classList.remove('animate__fadeInDown')
-
-		}, 1000)//remove the classes in case of next animation
 	})
 	function hideNav() {
+		navbar.removeAttribute('active')//if user to closes the nav
+		nav.classList.remove('animate__fadeInRight')
 
-		nav.classList.add('animate__backOutDown')
-
-		ovlay.classList.remove('animate__slideInRight')
+		ovlay.classList.remove('animate__fadeIn')
 		ovlay.classList.add('animate__fadeOut')
-
-		setTimeout(()=>{
-			ovlay.classList.add('animate__fadeOut')
-			navbar.classList.remove('animate__fadeOut')
-		}, 200)
 
 		setTimeout(()=>{
 			ovlay.classList.remove('animate__fadeOut')
